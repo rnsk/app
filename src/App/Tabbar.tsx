207ca163874defa0bd64976f5cc0119d63a7afe3
rnsk <touch@rnsk.net>
2020-05-06 11:10:30 +0900

    タブバーのアイコンと文言を修正

--- src/App/Tabbar.tsx
+++ src/App/Tabbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Tabbar.scss'
 import config from '../config.json'
 
-import { FaList, FaHome, FaImage } from "react-icons/fa"
+import { FaList, FaHome, FaUtensils } from "react-icons/fa"
 import { GiJapan } from "react-icons/gi"
 import { AiOutlineAppstore } from "react-icons/ai"
 
@@ -11,15 +11,15 @@ const Content = () => {
     <div className="tabbar">
       <ul>
         <li><a href="/#"><div className="icon"><FaHome /></div><div className="text">ホーム</div></a></li>
-        <li><a href="/#list"><div className="icon"><FaList /></div><div className="text">一覧</div></a></li>
+        <li><a href="/#list"><div className="icon"><FaList /></div><div className="text">お店一覧</div></a></li>
         {config.gallery_url?
           <>
-            <li><a href="/#gallery"><div className="icon"><FaImage /></div><div className="text">ギャラリー</div></a></li>
+            <li><a href="/#gallery"><div className="icon"><FaUtensils /></div><div className="text">料理から探す</div></a></li>
           </>
           :
           <></>
         }
-        <li><a href="https://iemeshi.jp/"><div className="icon"><GiJapan /></div><div className="text">全国</div></a></li>
+        <li><a href="https://iemeshi.jp/"><div className="icon"><GiJapan /></div><div className="text">全国で探す</div></a></li>
         <li><a href="/#about"><div className="icon"><AiOutlineAppstore /></div><div className="text">イエメシとは</div></a></li>
       </ul>
     </div>
